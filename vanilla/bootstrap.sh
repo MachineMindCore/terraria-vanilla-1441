663f0bcbe1817567d6f81563c3056077ef462689
unknown <user.name>
2024-11-12 22:37:00 -0500

    fix dockerfile

--- vanilla/bootstrap.sh
+++ vanilla/bootstrap.sh
@@ -18,7 +18,7 @@ if [ -z "$WORLD_FILENAME" ]; then
   else
     echo "Running server with command flags: $@"
   fi
-  mono TerrariaServer.exe -config "$CONFIGPATH/$CONFIG_FILENAME" -logpath "$LOGPATH" "$@" "-world" "/root/.local/share/Terraria/Worlds/nacion_burra.wld" "-autocreate", "3"
+  mono TerrariaServer.exe -config "$CONFIGPATH/$CONFIG_FILENAME" -logpath "$LOGPATH" "$@"
 else
   echo "Environment WORLD_FILENAME specified"
   if [ -f "$WORLD_PATH" ]; then
